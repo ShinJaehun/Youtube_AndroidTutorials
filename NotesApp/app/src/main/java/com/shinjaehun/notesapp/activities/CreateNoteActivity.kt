0869fa6b3e52fd05d76ef65375e56a4dfa1bbbc5
ShinJaehun <gunsmoke.shin@gmail.com>
2023-08-21 13:35:30 +0900

    NotesApp : create and save notes

--- NotesApp/app/src/main/java/com/shinjaehun/notesapp/activities/CreateNoteActivity.kt
+++ NotesApp/app/src/main/java/com/shinjaehun/notesapp/activities/CreateNoteActivity.kt
@@ -1,8 +1,17 @@
 package com.shinjaehun.notesapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.shinjaehun.notesapp.database.NotesDatabase
 import com.shinjaehun.notesapp.databinding.ActivityCreateNoteBinding
+import com.shinjaehun.notesapp.entities.Note
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CreateNoteActivity : AppCompatActivity() {
 
@@ -14,6 +23,46 @@ class CreateNoteActivity : AppCompatActivity() {
 
         activityCreateNoteBinding.imageBack.setOnClickListener {
             onBackPressed()
+        }
+
+        activityCreateNoteBinding.textDateTime.text = SimpleDateFormat("yyyy MMMM dd, EEEE, HH:mm a", Locale.getDefault()).format(Date())
+
+        activityCreateNoteBinding.imageSave.setOnClickListener {
+            saveNote()
+        }
+    }
+
+    private fun saveNote() {
+        val title = activityCreateNoteBinding.etNoteTitle.text.toString()
+        val subtitle = activityCreateNoteBinding.etNoteSubtitle.text.toString()
+        val noteText = activityCreateNoteBinding.etNote.text.toString()
+        val dateTime = activityCreateNoteBinding.textDateTime.text.toString()
+
+
+        if (title.trim().isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty", Toast.LENGTH_SHORT).show()
+            return
+        } else if (subtitle.trim().isEmpty()) {
+            Toast.makeText(this, "Note subtitle can't be empty", Toast.LENGTH_SHORT).show()
+            return
+        } else if (noteText.trim().isEmpty()) {
+            Toast.makeText(this, "Notes can't be empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        var note = Note(
+            title = title,
+            subtitle = subtitle,
+            noteText = noteText,
+            dateTime = dateTime
+        )
+
+        GlobalScope.launch {
+            NotesDatabase.getDatabase(applicationContext)?.noteDao()!!.insertNote(note).also {
+                val intent = Intent()
+                setResult(RESULT_OK, intent)
+                finish()
+            }
         }
     }
 }
--- NotesApp/app/src/main/java/com/shinjaehun/notesapp/activities/MainActivity.kt
+++ NotesApp/app/src/main/java/com/shinjaehun/notesapp/activities/MainActivity.kt
@@ -3,7 +3,13 @@ package com.shinjaehun.notesapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.shinjaehun.notesapp.database.NotesDatabase
 import com.shinjaehun.notesapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,4 +24,19 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(Intent(applicationContext, CreateNoteActivity::class.java), REQUEST_CODE_ADD_NOTE)
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        getNotes() // 얘가 onCreate에 있으면... 
+    }
+
+    private fun getNotes() {
+        GlobalScope.launch {
+            NotesDatabase.getDatabase(applicationContext)?.noteDao()!!.getAllNotes().also {
+                Log.i(TAG, it.toString())
+            }
+        }
+    }
+
+
 }
